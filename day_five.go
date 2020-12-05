1f44d4a6fa9e3605cf14af51724fa6b9d36b1a1c
alec.tunbridge <alec.tunbridge@gmail.com>
2020-12-05 11:42:48 +0000

    part 1 complete

--- day_five.go
+++ day_five.go
@@ -13,6 +13,29 @@ func Split(min int, max int, character string) (int, int) {
 	return min+(max-min+2-1)/2, max
 }
 
+func ProcessInputLine(line string) (int, int, int){
+	var minRow = 0
+	var maxRow = 127
+
+	for i:=0; i<7; i++{
+		minRow, maxRow = Split(minRow, maxRow, string(line[i]))
+	}
+
+	var minColumn = 0
+	var maxColumn = 7
+	for j:=7; j<10; j++{
+		minColumn, maxColumn = Split(minColumn, maxColumn, string(line[j]))
+	}
+
+	if minRow != maxRow || minColumn != maxColumn {
+		panic(fmt.Sprintf(`something went wrong,
+		our binary search didn't converge %d %d %d %d`,
+		minRow, maxRow, minColumn, maxColumn))
+	}
+
+	return minRow, minColumn, minRow*8+minColumn 
+}
+
 func main(){
 	file, err := os.Open("day_five.txt")
 	if err != nil {
@@ -21,7 +44,12 @@ func main(){
 
 	scanner := bufio.NewScanner(file)
 
+	var maxId=0
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		_, _, id := ProcessInputLine(scanner.Text())
+		if id > maxId {
+			maxId = id
+		}
 	}
+	fmt.Printf("maxId: %d", maxId)
 }
--- day_five_test.go
+++ day_five_test.go
@@ -17,7 +17,7 @@ func TestSplit(t *testing.T) {
 		{40,47,"B",44,47},
 		{44,47,"F",44,45},
 		{44,45,"F",44,44},
-		
+
 		{0,7,"R",4,7},
 		{4,7,"L",4,5},
 		{4,5,"R",5,5},
@@ -35,6 +35,36 @@ func TestSplit(t *testing.T) {
 			table.min, table.max, table.character,
 			table.minOut, table.maxOut,
 			minValue, maxValue)
+		}
+	}
+}
+
+
+func TestProcessInputLine(t *testing.T) {
+	tables := []struct {
+		input string
+		expectedRow int
+		expectedColumn int
+		expectedId int
+	}{
+		{"BFFFBBFRRR",70,7,567},
+		{"FFFBBBFRRR",14,7,119},
+		{"BBFFBBFRLL",102,4,820},
+	}
+
+	for _, table := range tables {
+		row, column, id := ProcessInputLine(table.input)
+
+		if row != table.expectedRow ||
+		column != table.expectedColumn ||
+		id != table.expectedId {
+			t.Errorf(`ProcessInputLine went wrong
+			given: %s
+			expected: row:%d column:%d id:%d
+			got: row:%d column:%d id:%d`,
+			table.input,
+			table.expectedRow, table.expectedColumn, table.expectedId,
+			row, column, id)
 		}
 	}
 }
