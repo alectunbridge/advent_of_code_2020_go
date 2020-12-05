2c823848c2baea73f8fac4355ce91a0089710d03
alec.tunbridge <alec.tunbridge@gmail.com>
2020-12-05 10:42:42 +0000

    add rudimentary file reading

--- day_five.go
+++ day_five.go
@@ -1,4 +1,9 @@
 package main
+import (
+	"os"
+	"bufio"
+	"fmt"
+)
 
 func Split(min int, max int, character string) (int, int) {
 	if character == "F" || character == "L" {
@@ -6,4 +11,17 @@ func Split(min int, max int, character string) (int, int) {
 	}
 
 	return min+(max-min+2-1)/2, max
+}
+
+func main(){
+	file, err := os.Open("day_five.txt")
+	if err != nil {
+		panic("Can't read the file")
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 }
